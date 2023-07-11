9e1bada2967f7b36f83dcb022e9810ad08d3a9e7
ollihansen90 <50058898+ollihansen90@users.noreply.github.com>
2023-07-11 11:48:29 +0200

    Create utils_gan.py

---
+++ utils/utils_gan.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self, nz):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(128, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 512),
+            nn.LeakyReLU(0.2),
+            nn.Linear(512, 1024),
+            nn.LeakyReLU(0.2),
+            nn.Linear(1024, 784),
+            nn.Tanh(),
+        )
+
+    def forward(self, x):
+        return torch.reshape(self.net(x), (-1, 1, 28, 28))
